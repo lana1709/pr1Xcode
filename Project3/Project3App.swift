28f0bd380297c124bea57979a71e16caac4384fb
lana <user7@r.r>
2021-06-07 10:17:12 +0300

    Initial Commit

---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by user on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
